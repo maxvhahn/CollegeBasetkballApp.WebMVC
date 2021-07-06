68b75ed50ff7a4721e195bc91af770fbf8892b1a
Max Hahn <max.vhahn@gmail.com>
2021-07-06 19:47:32 -0400

    Add project files.

---
+++ CollegeBasetkballApp.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CollegeBasetkballApp.WebMVC.Startup))]
+namespace CollegeBasetkballApp.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
