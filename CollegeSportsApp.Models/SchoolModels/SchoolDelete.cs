17faa04c1d15b7fea94bb0c5e57322c0dde71b90
Max Hahn <max.vhahn@gmail.com>
2021-07-08 21:24:25 -0400

    Built School Models CRUD and added address to School Data Table

---
+++ CollegeSportsApp.Models/SchoolModels/SchoolDelete.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollegeSportsApp.Models.SchoolModels
+{
+    public class SchoolDelete
+    {
+        public int SchoolId { get; set; }
+    }
+}
---
+++ CollegeSportsApp.Models/SchoolModels/SchoolDetail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollegeSportsApp.Models.SchoolModels
+{
+    public class SchoolDetail
+    {
+        public int SchoolId { get; set; }
+        public string SchoolName { get; set; }
+        public string MascotName { get; set; }
+        public string Address
+        {
+            get
+            {
+                return (City + " " + State);
+            }
+        }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string State { get; set; }
+    }
+}
