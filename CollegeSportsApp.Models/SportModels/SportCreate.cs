f8a00ed324ea0d45b2be76ba919d77a083b2e277
Max Hahn <max.vhahn@gmail.com>
2021-07-10 09:40:37 -0400

    Added SportDetail and Annotations to SportCreate

--- CollegeSportsApp.Models/SportModels/SportCreate.cs
+++ CollegeSportsApp.Models/SportModels/SportCreate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,11 @@ namespace CollegeSportsApp.Models.SportModels
     public class SportCreate
     {
         public int SportId { get; set; }
+        [Required]
         public string SportName { get; set; }
+
+        [Required]
+        public string SportDescription { get; set; }
         public int SchoolId { get; set; }
     }
 }
