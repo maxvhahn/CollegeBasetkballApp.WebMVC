433d4beedbaefcefb8d99f6da9d5e05acee1c8a0
Max Hahn <max.vhahn@gmail.com>
2021-07-15 19:43:52 -0400

    Completed Controller Build

---
+++ CollegeSportsApp.Data/Migrations/202107152339518_FinishedControllers.cs
@@ -0,0 +1,16 @@
+namespace CollegeSportsApp.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FinishedControllers : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
